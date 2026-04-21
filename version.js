539690cdbb9e93445dbd37e0172e2bbc185e74ab
Peter Eccles <pjeccles@gmail.com>
2026-04-22 00:09:57 +1000

    Commit a version number so the app is runnable without changes.

---
+++ version.js
@@ -0,0 +1,6 @@
+// version.template.js: Template for version string injection
+
+export async function getVersion() {
+  // This will be replaced by build script
+  return 'janktris-v0.2.1'; // e.g. janktris-v0.2.0-3-g8967625-dirty
+}
